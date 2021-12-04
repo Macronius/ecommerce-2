a0f15c346f0058a011e0c1a90a770dd95ff7a421
Marcus <marconiushenry@gmail.com>
2021-12-04 15:45:59 -0600

    added addItem and removeItem functionality to checkout-item component

--- src/components/checkout-item/checkout-item.component.jsx
+++ src/components/checkout-item/checkout-item.component.jsx
@@ -1,21 +1,58 @@
 import React from 'react'
 import './checkout-item.styles.scss'
 
+//NOTE: bind the redux action to the component via 'connect'
+import {connect} from 'react-redux'
+
+import {clearItemFromCart, addItem, removeItem} from '../../redux/cart/cart.actions'
+
 
 //NOTE: for props reference, see cart.actions (sends the entire item)
-const CheckoutItem = ({cartItem: {name, imageUrl, price, quantity}})=> {
+const CheckoutItem = ({cartItem, clearItem, addItem, removeItem})=> {
+
+    const {name, imageUrl, price, quantity} = cartItem
 
     return(
         <div className="checkout-item">
             <div className="image-container">
                 <img src={imageUrl} alt="item"/>
             </div>
+
             <span className="name">{name}</span>
-            <span className="quantity">{quantity}</span>
+            <span className="quantity">
+                <div className="arrow" onClick={()=> removeItem(cartItem) }>
+                    &#10094;
+                </div>
+                    <span className="value">{quantity}</span>
+                <div className="arrow" onClick={ ()=> addItem(cartItem)}>
+                    &#10095;
+                </div>
+            </span>
             <span className="price">{price}</span>
-            <div className="remove-button">&#10005;</div>
+
+            <div 
+                className="remove-button"
+                onClick={()=> clearItem(cartItem)}
+            >
+                &#10005;
+            </div>
         </div>
     )
 }
 
-export default CheckoutItem
+
+
+const mapDispatchToProps = (dispatch)=> (
+    {
+        clearItem: (item)=> dispatch(clearItemFromCart(item)),
+        addItem: (item)=> dispatch(addItem(item)),
+        removeItem: (item)=> dispatch(removeItem(item))
+    }
+)
+/*
+QUESTIONS:
+    - where did the name 'clearItem' come from?
+    - is clearItem now the function (aka dispatched action)?
+*/
+
+export default connect(null, mapDispatchToProps)(CheckoutItem)
--- src/redux/cart/cart.utils.js
+++ src/redux/cart/cart.utils.js
@@ -15,3 +15,27 @@ export const addItemToCart = (cartItems, cartItemToAdd)=> {
     //if cartItem is not already found in the array, return new array with all existing plus new
     return [...cartItems, {...cartItemToAdd, quantity: 1}]
 }
+
+
+
+export const removeItemFromCart = (cartItems, cartItemToRemove)=> {
+    const existingCartItem = cartItems.find( 
+        (cartItem)=> cartItem.id === cartItemToRemove.id
+    )
+
+    console.log(`cartItemToRemove: ${cartItemToRemove}`)
+    console.log(`existingCartItem: ${existingCartItem}`)
+
+    if(existingCartItem.quantity === 1) {
+        return cartItems.filter(
+            (cartItem)=> cartItem.id !== cartItemToRemove.id
+        )
+        //QUESTION: why use cartItemToRemove over existingCartItem?
+    }
+
+    return cartItems.map(
+        (cartItem)=> cartItem.id === cartItemToRemove.id
+            ? {...cartItem, quantity: cartItem.quantity -1}
+            : cartItem
+    )
+}
