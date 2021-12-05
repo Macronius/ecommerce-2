a4f641a7f7b348db931548cd6f7f86e4793720a2
Marcus <marconiushenry@gmail.com>
2021-12-04 19:34:06 -0600

    implemented redux persist on the cart to enable local storage

--- src/redux/root-reducer.js
+++ src/redux/root-reducer.js
@@ -1,11 +1,27 @@
 //the base reducer that represents all of the state of the application
 
 import {combineReducers} from 'redux'
+import {persistReducer} from 'redux-persist'
+import storage from 'redux-persist/lib/storage'  // window.localStorage
+//import sessionStorage from 'redux-persist/lib/session-storage' //just a guess url, see doc
 
 import userReducer from './user/user.reducer'
 import cartReducer from './cart/cart.reducer'
 
-export default combineReducers({
-    user: userReducer,
-    cart: cartReducer,
-})
+//define a new persist config
+const persistConfig = {
+    key: 'root',
+    storage,
+    whitelist: ['cart']//an array containing string names of any reducers we want to store
+}
+
+const rootReducer = combineReducers(
+    {
+        user: userReducer,
+        cart: cartReducer
+    }
+)
+
+
+
+export default persistReducer(persistConfig, rootReducer)
